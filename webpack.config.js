3795d9cb4ac8687f024f8b2cf29ff6a959818643
Ace Subido <ace.subido@gmail.com>
2018-08-02 00:01:34 +0800

    Format single quote strings

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,24 @@
 // Webpack v4
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const CleanWebpackPlugin = require('clean-webpack-plugin')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-const StylelintWebpackPlugin = require('stylelint-webpack-plugin')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const StylelintWebpackPlugin = require("stylelint-webpack-plugin");
 
-const DEBUG = process.env.NODE_ENV != 'production'
-const SOURCE = './src'
+const DEBUG = process.env.NODE_ENV != "production";
+const SOURCE = "./src";
 
 module.exports = {
-
   entry: {
-    'assets/js/main': `${SOURCE}/js/index.js`,
-    'assets/css/main': `${SOURCE}/css/index.js`
+    "assets/js/main": `${SOURCE}/js/index.js`,
+    "assets/css/main": `${SOURCE}/css/index.js`
   },
 
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js'
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+    devtoolModuleFilenameTemplate: "webpack:///[absolute-resource-path]"
   },
 
   module: {
@@ -27,31 +27,33 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
+          loader: "babel-loader"
         }
       },
       {
         test: /\.scss$/,
         use: [
-          'style-loader',
+          "style-loader",
           MiniCssExtractPlugin.loader,
-          'css-loader',
-          'postcss-loader',
-          'sass-loader'
+          "css-loader",
+          "postcss-loader",
+          "sass-loader"
         ]
       }
     ]
   },
 
   plugins: [
-    new MiniCssExtractPlugin({ filename: 'assets/css/style.[contenthash].css' }),
+    new MiniCssExtractPlugin({
+      filename: "assets/css/style.[contenthash].css"
+    }),
     new HtmlWebpackPlugin({
       hash: true,
-      template: './src/html/index.html',
-      filename: 'index.html'
+      template: "./src/html/index.html",
+      filename: "index.html"
     }),
-    new CleanWebpackPlugin('dist', {}),
-    new CopyWebpackPlugin([{ from: `${SOURCE}/assets`, to: './assets' }]),
+    new CleanWebpackPlugin("dist", {}),
+    new CopyWebpackPlugin([{ from: `${SOURCE}/assets`, to: "./assets" }]),
     new StylelintWebpackPlugin({ syntax: "scss" })
   ],
 
